a4d7d0ae816fad1bd88ee0baf5c566f463eeadeb
changjian01 <768025549@qq.com>
2017-06-03 00:29:27 +0800

    fix

--- build_using_gym.sh
+++ build_using_gym.sh
@@ -4,22 +4,24 @@ SECONDS=0
 project_path=$(pwd)
 #取当前时间字符串添加到文件结尾
 now=$(date +"%Y_%m_%d_%H_%M_%S")
+#项目名称
+appName="GitSearch"
 #指定项目的scheme名称
-scheme="GitSearch"
+scheme=appName
 #指定要打包的配置名
 configuration="enterprise"
 #指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, development, 和developer-id，即xcodebuild的method参数
 export_method='enterprise'
 #指定项目地址
-workspace_path="$project_path/scheme.xcworkspace"
+workspace_path="$project_path/${appName}.xcworkspace"
 #指定输出路径
-output_path="/Users/Desktop/scheme/"
+output_path="/Users/Desktop/${appName}/"
 #指定输出归档文件地址
-archive_path="$output_path/scheme_${now}.xcarchive"
+archive_path="$output_path/${appName}_${now}.xcarchive"
 #指定输出ipa地址
-ipa_path="$output_path/scheme_${now}.ipa"
+ipa_path="$output_path/${appName}_${now}.ipa"
 #指定输出ipa名称
-ipa_name="scheme_${now}.ipa"
+ipa_name="${appName}_${now}.ipa"
 #获取执行命令时的commit message
 commit_msg="$1"
 #输出设定的变量值

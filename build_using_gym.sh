334add0f499be7204e1b969202db75d98584366d
changjian01 <768025549@qq.com>
2017-06-03 01:38:16 +0800

    12112

--- build_using_gym.sh
+++ build_using_gym.sh
@@ -1,43 +1,48 @@
+#!/bin/bash
+
+#设置超时
+export FASTLANE_XCODEBUILD_SETTINGS_TIMEOUT=120
+
 #计时
 SECONDS=0
+
 #假设脚本放置在与项目相同的路径下
 project_path=$(pwd)
 #取当前时间字符串添加到文件结尾
 now=$(date +"%Y_%m_%d_%H_%M_%S")
-#项目名称
-appName="GitSearch"
+
 #指定项目的scheme名称
-scheme=appName
+scheme="GitSearch"
 #指定要打包的配置名
-configuration="Distribution"
-#指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, devel
-opment, 和developer-id，即xcodebuild的method参数
+configuration="enterprise"
+#指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, development, 和developer-id，即xcodebuild的method参数
 export_method='enterprise'
-provisioning_profile='IM_Distribution.mobileprovision'
 
 #指定项目地址
-workspace_path="$project_path/${appName}.xcworkspace"
+workspace_path="$project_path/GitSearch.xcworkspace"
 #指定输出路径
-output_path="${HOME}/Desktop/${appName}"
+output_path="${HOME}/Desktop/ipa"
 #指定输出归档文件地址
-archive_path="$output_path/${appName}_${now}.xcarchive"
+archive_path="$output_path/GitSearch_${now}.xcarchive"
 #指定输出ipa地址
-ipa_path="$output_path/${appName}_${now}.ipa"
+ipa_path="$output_path/GitSearch_${now}.ipa"
 #指定输出ipa名称
-ipa_name="${appName}_${now}.ipa"
+ipa_name="GitSearch_${now}.ipa"
 #获取执行命令时的commit message
 commit_msg="$1"
+
 #输出设定的变量值
 echo "===workspace path: ${workspace_path}==="
 echo "===archive path: ${archive_path}==="
 echo "===ipa path: ${ipa_path}==="
 echo "===export method: ${export_method}==="
 echo "===commit msg: $1==="
+
 #先清空前一次build
-gym --workspace ${workspace_path} --scheme ${scheme} --clean --configuration ${configuration} --archive_path ${archive_path} --export_method ${export_method} --output_directory ${output_path} --output_name ${ipa_name}
+fastlane gym --workspace ${workspace_path} --scheme ${scheme} --clean --configuration ${configuration} --archive_path ${archive_path} --export_method ${export_method} --output_directory ${output_path} --output_name ${ipa_name}
+
 #上传到fir
 fir publish ${ipa_path} -T 863efefc2c22d4b761c096e6af9a6024 -c "${commit_msg}"
+
 #输出总用时
 echo "===Finished. Total time: ${SECONDS}s==="
-
-

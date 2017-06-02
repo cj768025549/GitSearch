26eece0b2b1a6ce6320198ff51c6fcc4a6970dd4
changjian01 <768025549@qq.com>
2017-06-02 23:47:48 +0800

    fix

--- build_using_xctool.sh
+++ build_using_xctool.sh
@@ -7,9 +7,9 @@ project_infoplist_path="./${APP_NAME}/Info.plist"
 #取版本号
 bundleShortVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "${project_infoplist_path}")
 #取build值
-bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_infoplist_path}")
-DATE="$(date +%Y%m%d)"
-FILENAME="${APP_NAME}_V${bundleShortVersion}_${DATE}"
+#bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_infoplist_path}")
+DATE="$(date +%Y%m%d%H%m%s)"
+FILENAME="${APP_NAME}_${DATE}"
 IPANAME="${APP_NAME}.ipa"
 #要上传的ipa文件路径
 IPA_PATH="$HOME/${IPANAME}"
@@ -22,9 +22,9 @@ xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}"  -configur
 echo "+++++++++++++++++build+++++++++++++++++"
 xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -sdk iphoneos -configuration 'Release' CODE_SIGN_IDENTITY="${CODE_SIGN_DISTRIBUTION}" SYMROOT='$(PWD)'
 echo "+++++++++++++++++archive+++++++++++++++++"
-xcodebuild -workspace ${APP_NAME}.xcworkspace -scheme "${APP_NAME}" -configuration Release -archivePath ./build/${FILENAME}.xcarchive archive
+xcodebuild -workspace ${APP_NAME}.xcworkspace -scheme "${APP_NAME}" -configuration Release -archivePath ./build/${FILENAME}/${FILENAME}.xcarchive archive
 echo "+++++++++++++++++导出IPA文件++++++++++++++++++"
-xcodebuild -exportArchive -archivePath ./build/${FILENAME}.xcarchive -exportPath ./build/${FILENAME}  -exportOptionsPlist ./build/export_info.plist
+xcodebuild -exportArchive -archivePath ./build/${FILENAME}/${FILENAME}.xcarchive -exportPath ./build/${FILENAME}/${FILENAME}  -exportOptionsPlist ./build/export_info.plist
 echo "+++++++++++++++++上传ipa至fir++++++++++++++++++"
 
 echo "正在上传到fir.im...."

15bb1dace0a393b9cc472e7b1b819525b0c18cfb
changjian01 <768025549@qq.com>
2017-06-03 11:50:53 +0800

    guy

--- build_using_xctool.sh
+++ build_using_xctool.sh
@@ -2,7 +2,7 @@ export LANG=en_US.UTF-8     #   执行pod install时用到
 security unlock-keychain "-p" "12"   # MAC授权密码
 appname="GitSearch"
 codesignidentify="iPhone Distribution: beijing Tongchengtong Information Technology Co., Ltd"   # 需要指定证书
-provisoningprofile="4c419047-9224-4cf7-a328-0248e3e93d8d"
+provisoningprofile="2bb39949-c853-4f78-a4c5-106928265908"
 
 projectpath=$(pwd)
 basepath=$HOME

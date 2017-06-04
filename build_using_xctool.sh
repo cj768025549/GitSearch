490dacc0862abb9a14239e0d0d5f51b05308ef30
changjian01 <768025549@qq.com>
2017-06-04 21:33:50 +0800

    122112212

--- build_using_xctool.sh
+++ build_using_xctool.sh
@@ -3,7 +3,7 @@ security unlock-keychain -p 12 /Users/changjian/Library/keychains/login.keychain
 APP_NAME="GitSearch"
 # 证书
 CODE_SIGN_DISTRIBUTION="iPhone Distribution: beijing Tongchengtong Information Technology Co., Ltd"
-provisoningprofile="IM_Distribution"
+provisoning_profile="IM_Distribution"
 
 # info.plist路径
 project_infoplist_path="./${APP_NAME}/Info.plist"
@@ -14,6 +14,8 @@ bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_in
 DATE="$(date +%Y%m%d%H%m)"
 FILENAME="${DATE}"
 FIRTOKEN = "863efefc2c22d4b761c096e6af9a6024"
+FIRAPPID = "5933aa88548b7a57ff000059"
+
 #IPANAME="${APP_NAME}.ipa"
 #要上传的ipa文件路径
 #IPA_PATH="$HOME/Desktop/${IPANAME}"
@@ -29,12 +31,12 @@ xcodebuild -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME}  -configuratio
 
 
 echo "+++++++++++++++++build+++++++++++++++++"
-xcodebuild archive -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -configuration 'Release' -archivePath "./build/${FILENAME}/${APP_NAME}.xcarchive" CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoningprofile"
+xcodebuild archive -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -configuration 'Release' -archivePath "./build/${FILENAME}/${APP_NAME}.xcarchive" CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoning_profile"
 
 echo "+++++++++++++++++archive+++++++++++++++++"
-#xcodebuild archive -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME} -configuration 'Release' -archivePath ./build/${FILENAME}/${APP_NAME}.xcarchive CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoningprofile"
+#xcodebuild archive -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME} -configuration 'Release' -archivePath ./build/${FILENAME}/${APP_NAME}.xcarchive CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoning_profile"
 echo "+++++++++++++++++导出IPA文件++++++++++++++++++"
-xcodebuild -exportArchive -archivePath "./build/${FILENAME}/${APP_NAME}.xcarchive" -exportPath "./build/${FILENAME}"  -exportOptionsPlist ./export_info.plist CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoningprofile"
+xcodebuild -exportArchive -archivePath "./build/${FILENAME}/${APP_NAME}.xcarchive" -exportPath "./build/${FILENAME}"  -exportOptionsPlist ./export_info.plist CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoning_profile"
 echo "+++++++++++++++++上传ipa至fir++++++++++++++++++"
 
 echo "正在上传到fir.im...."
@@ -42,8 +44,10 @@ echo "正在上传到fir.im...."
 fir p "./build/${FILENAME}/${APP_NAME}.ipa"
 changelog=`cat $projectDir/README`
 
-curl -X PUT --data "changelog=$changelog" http://fir.im/api/v2/app/59313172548b7a16a9000264?token=${FIRTOKEN}
+curl -X PUT --data "changelog=$changelog" http://fir.im/api/v2/app/${FIRAPPID}?token=${FIRTOKEN}
 echo "\n打包上传更新结束"
+
+open .
 #fir无法使用时，上传至蒲公英
 
 #fastlane gym --export_method enterprise --output_name ${APP_NAME}

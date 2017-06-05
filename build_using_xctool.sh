66aca384b644502e2cb3963cad2d9a2b34b0442c
cj768025549 <768025549@qq.com>
2017-06-05 16:19:37 +0800

    212112

--- build_using_xctool.sh
+++ build_using_xctool.sh
@@ -11,16 +11,10 @@ project_infoplist_path="./${APP_NAME}/Info.plist"
 bundleShortVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleShortVersionString" "${project_infoplist_path}")
 #取build值
 bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_infoplist_path}")
-DATE="$(date +%Y%m%d%H%m)"
+DATE="$(date +%Y_%m_%d_%H_%M)"
 FILENAME="${DATE}"
 FIRTOKEN = "863efefc2c22d4b761c096e6af9a6024"
 FIRAPPID = "5933aa88548b7a57ff000059"
-
-#IPANAME="${APP_NAME}.ipa"
-#要上传的ipa文件路径
-#IPA_PATH="$HOME/Desktop/${IPANAME}"
-#echo ${IPA_PATH}
-#echo "${IPA_PATH}">> text.txt
 
 echo "=================pod install================="
 pod install
@@ -33,14 +27,13 @@ xcodebuild -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME}  -configuratio
 echo "+++++++++++++++++build+++++++++++++++++"
 xcodebuild archive -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -configuration 'Release' -archivePath "./build/${FILENAME}/${APP_NAME}.xcarchive" CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoning_profile"
 
-echo "+++++++++++++++++archive+++++++++++++++++"
-#xcodebuild archive -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME} -configuration 'Release' -archivePath ./build/${FILENAME}/${APP_NAME}.xcarchive CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoning_profile"
-echo "+++++++++++++++++导出IPA文件++++++++++++++++++"
+echo "+++++++++++++++++archive && 导出ipa文件++++++++++++++++++"
 xcodebuild -exportArchive -archivePath "./build/${FILENAME}/${APP_NAME}.xcarchive" -exportPath "./build/${FILENAME}"  -exportOptionsPlist ./export_info.plist CODE_SIGN_IDENTITY="$CODE_SIGN_DISTRIBUTION" PROVISIONING_PROFILE="$provisoning_profile"
-echo "+++++++++++++++++上传ipa至fir++++++++++++++++++"
+echo "+++++++++++++++++复制到桌面++++++++++++++++++"
+mkdir ${HOME}/Desktop/Package
+cp -R ./build/${FILENAME}/${APP_NAME}.ipa ${HOME}/Desktop/Package
 
-echo "正在上传到fir.im...."
-fir p "./build/${FILENAME}/${APP_NAME}.ipa" -T "${FIRTOKEN}" -Q
+#fir p "./build/${FILENAME}/${APP_NAME}.ipa" -T "${FIRTOKEN}" -Q
 #fir login ${FIRTOKEN}
 #echo ./build/${FILENAME}/${APP_NAME}.ipa
 #fir publish ./build/${FILENAME}/${APP_NAME}.ipa
@@ -49,7 +42,6 @@ fir p "./build/${FILENAME}/${APP_NAME}.ipa" -T "${FIRTOKEN}" -Q
 #curl -X PUT  --data "changelog=$changelog" http://fir.im/api/v2/app/5933aa88548b7a57ff000059?token=863efefc2c22d4b761c096e6af9a6024
 #echo "\n打包上传更新结束"
 
-open .
 #fir无法使用时，上传至蒲公英
 
-curl -F "file=./build/${FILENAME}/${APP_NAME}.ipa" -F "uKey=d81326899dd50c3382e2f5e99f3a7495" -F "_api_key=495642f9b1336a64ceb2d5cb44d93183" "http://www.pgyer.com/apiv1/app/upload"
+#curl -F "file=./build/${FILENAME}/${APP_NAME}.ipa" -F "uKey=d81326899dd50c3382e2f5e99f3a7495" -F "_api_key=495642f9b1336a64ceb2d5cb44d93183" "http://www.pgyer.com/apiv1/app/upload"
